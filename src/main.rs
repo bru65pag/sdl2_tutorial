bdafea17cbf3a2274a450ea05dcef5b67c608eba
Pagis <bru65pag@gmail.com>
2020-03-15 21:49:41 +0700

    part 3 implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 extern crate sdl2;
 
+use std::convert::TryInto;
 use std::{thread, time};
 use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard;
+// use sdl2::rect::Point;
+use sdl2::rect::Rect;
+// use sdl2::render::Canvas;
+// use std::time::Duration;
+
+// struct Player {
+//     pos_x:i32,
+//     pos_y:i32,
+//     size_x:u32,
+//     size_y:u32
+// }
+//
+// impl Default for Player {
+//     fn default() -> Player {
+//         Player {pos_x: 20,pos_y:20,size_x:20,size_y:20}
+//     }
+// }
 
 fn main() {
+    let sleep_time = time::Duration::from_millis(16);
+    let mut player = Rect::new(20,20,20,20);
     let pos_x:i32 = 100;
     let pos_y:i32 = 200;
     let size_x:u32 = 300;
@@ -19,10 +41,57 @@ fn main() {
         .present_vsync()
         .build()
         .unwrap();
-    canvas.set_draw_color(Color::RGB(255,0,0));
-    canvas.clear();
-    canvas.present();
+    let mut running:bool = true;
+    while running {
+        let mut event_pump = sdl.event_pump().unwrap();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => {
+                    running = false;
+                }
+                Event::KeyDown { timestamp: _, window_id: _,
+                keycode , scancode: _, keymod: _, repeat: _ } => {
+                    match keycode.unwrap() {
+                        keyboard::Keycode::Right => {
+                            if player.x + player.w <  size_x.try_into().unwrap() {
+                                player.x += 1;
+                            }
+                        }
+                        keyboard::Keycode::Left => {
+                            if player.x > 0 {
+                                player.x -= 1;
+                            }
+                        }
+                        keyboard::Keycode::Down => {
+                            if player.y + player.h < size_y.try_into().unwrap() {
+                                player.y += 1;
+                            }
+                        }
+                        keyboard::Keycode::Up => {
+                            if player.y > 0 {
+                                player.y -= 1;
+                            }
+                        }
+                        keyboard::Keycode::Escape => {
+                            running = false;
+                        }
+                        _ => {
+                        }
+                    }
+                }
+                _ => {
+                }
+            }
+        }
+        // render
+        canvas.set_draw_color(Color::RGB(0,0,255));
+        canvas.clear();
 
-    let sleep_time = time::Duration::from_secs(4);
-    thread::sleep(sleep_time);
+        canvas.set_draw_color(Color::RGB(255,0,255));
+        canvas.fill_rect(player).unwrap();
+
+        canvas.present();
+
+        //thread::sleep(sleep_time);
+    }
 }
